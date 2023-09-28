59491e8a65e6c966379c1c436095242874ae16e0
Mst Surnaly Akter <surnalyms@gmail.com>
2023-09-28 19:34:34 +0600

    lesson8

---
+++ lesson8/index.js
@@ -0,0 +1,11 @@
+ const getRandomFruitsName = require("random-fruits-name");
+
+ console.log(getRandomFruitsName());
+ console.log(getRandomFruitsName("en")); // Apple
+console.log(getRandomFruitsName("es")); // Manzana
+console.log(getRandomFruitsName("ja")); // りんご
+console.log(getRandomFruitsName("pt")); // Maça
+console.log(getRandomFruitsName("nl")); // Appel
+console.log(getRandomFruitsName("fr")); // Pomme
+console.log(getRandomFruitsName("de")); // Apfel
+console.log(getRandomFruitsName("cs")); // Jablko
