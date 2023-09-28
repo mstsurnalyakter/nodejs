3ed0a4550e742eca7f4b125b90241f7f9f073a17
Mst Surnaly Akter <surnalyms@gmail.com>
2023-09-28 18:54:06 +0600

    server.js

---
+++ lesson6/sever.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const port = 3000;
+const hostName = '127.0.0.1';
+
+const server = http.createServer((req, res)=>{
+    res.writeHead(200, {'Content-Type':'text/html'});
+    return res.end('hello wordl')
+})
+
+server.listen(port,hostName,()=>{
+    console.log(`server is running at http://${hostName}:${port}`);
+})
